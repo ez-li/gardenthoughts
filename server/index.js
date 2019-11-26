996ad10688ea842a2e5f44e32d0b891a26dcec08
ez-li <eileenli.sf@gmail.com>
2019-11-26 11:57:47 -0800

    Building express server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const compression = require('compression');
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3003;
+
+const bodyParser = require('body-parser');
+
+app.use(compression());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', express.static(__dirname + '/../public'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.listen(port, function() {
+    console.log('Listening on Port 3003...');
+});
+
